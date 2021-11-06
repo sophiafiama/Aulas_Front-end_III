ee10095e70f8f638b33e5e5c36540436db20b9c8
sophiafiama <fiama.sophia.sf@gmail.com>
2021-11-06 18:15:41 -0300

    Atualizando conteudo da aula 0004

---
+++ Aulas_Front_III/Aula0004/my-app-name/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+        
+        <p>
+          TESTE         
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;

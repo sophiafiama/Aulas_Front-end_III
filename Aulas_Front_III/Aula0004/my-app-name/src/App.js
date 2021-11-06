1120ce85b805c311b71626789ae7c4528c0163e8
sophiafiama <fiama.sophia.sf@gmail.com>
2021-11-06 18:29:10 -0300

    Atualizando conteudo da aula 0004

--- Aulas_Front_III/Aula0004/my-app-name/src/App.js
+++ Aulas_Front_III/Aula0004/my-app-name/src/App.js
@@ -4,13 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header >
-        
+      <main>
+        <h1>
+          Mesa de Trabalho - Aula 4
+        </h1>
+        <h2>
+          Create-react-app
+        </h2>
         <p>
-          TESTE         
+          É a sua vez de fazer o teste. Agora que vimos como criar um aplicativo com create-react-app
+          e sabemos o que está dentro do aplicativo, vamos:
+
+          Faça nosso próprio aplicativo com npx create-react-app my-app-name.
+          Modifique os arquivos e limpe todos aqueles de que não precisamos.
+          Renderize a imagem do homem muito feliz por estar em um aplicativo limpo.
+          Suba seu projeto para a internet (Vercel, Netlify, etc).
+
         </p>
 
-      </header>
+      </main>
     </div>
   );
 }
